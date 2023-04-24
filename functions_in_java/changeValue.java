6d65e2783a5bd032f25be39ed98903f0c4f9b4f4
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2023-04-24 11:07:31 +0000

    TILLL

--- functions_in_java/changeValue.java
+++ functions_in_java/changeValue.java
@@ -1,8 +1,21 @@
 package functions_in_java;
 
+import java.util.Arrays;
+
 public class changeValue{
     public static void main(String[] args){
-        
+        // creating an array
+        int[] arr = {1 , 2 , 3};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
+
+    private static void change(int[] arr) {
+        
+
+    }
+
+    
 
 }
