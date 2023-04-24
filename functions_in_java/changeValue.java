364fbc1a99ad673cdd2d7b46807fecaef37612a0
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2023-04-24 13:03:21 +0000

    Done with change value -- passing value of reference variable.

--- functions_in_java/changeValue.java
+++ functions_in_java/changeValue.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 
 public class changeValue{
     public static void main(String[] args){
+
         // creating an array
-        int[] arr = {1 , 2 , 3};
+        int[] arr = {1 , 2 , 3, 5};
         change(arr);
         System.out.println(Arrays.toString(arr));
 
     }
 
-    private static void change(int[] arr) {
+    private static void change(int[] nums) {
+        nums[0] = 99; // if you make a change to the object via this ref variable, same object will be changed
         
-
     }
-
-    
 
 }
